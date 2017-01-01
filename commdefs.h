e956f2bac7d140a5405c3c678e397bfffa41ac28
Matteo Dessalvi <mdessalvi@gmail.com>
2017-01-01 13:22:48 +0000

    Include all function declarations on commdefs.h

--- commdefs.h
+++ commdefs.h
@@ -23,3 +23,19 @@ typedef enum {T_WORD, T_BAR, T_AMP, T_SEMI, T_GT, T_GTGT, T_LT, T_NL, T_EOF} TOK
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+
+/** Function declarations **/
+BOOLEAN builtin(int argc, char *argv[], int srcfd, int dstfd);
+void asg(int argc, char *argv[]);
+void set(int argc, char *argv[]);
+TOKEN command(int *waitpid, BOOLEAN makepipe,int *pipefdp);
+TOKEN gettoken(char *word);
+pid_t invoke(int argc, char *argv[], int srcfd, char *srcfile, int dstfd, char *dstfile, BOOLEAN append, BOOLEAN bckgrnd);
+void ignoresig();
+void entrysig();
+void redirect(int srcfd, char *srcfile, int dstfd, char *dstfile, BOOLEAN append, BOOLEAN bckgrnd);
+void shell_err(char *msg);
+void fatal(char *msg);
+void statusprt(pid_t pid, int status);
+void waitfor(pid_t pid);
+
